b86e9bc470dc4f2df531fe24524a4c37e02d28cc
Piotr Andruchowski <andrupio@gmail.com>
2021-04-14 20:37:04 +0200

    Zad.7.4

--- movie_library.py
+++ movie_library.py
@@ -30,3 +30,36 @@ s2 = Series(title="Chernobyl", year="2019", genre="Drama",  episodenumber="05", 
 s3 = Series(title="Game of Thrones", year="2013", genre="Drama, Fantasy, Adventure", episodenumber="01", seasonnumber="03")
 s4 = Series(title="Breaking Bad", year="2012", genre="Drama, Crime", episodenumber="01", seasonnumber="01")
 s5 = Series(title="Band of Brothers", year="2001", genre="War drama", episodenumber="09", seasonnumber="01")
+
+ms = [m1, m2, m3, m4, m5, s1, s2, s3, s4, s5]
+
+def get_movies():
+    by_title = sorted(ms, key=lambda Movies: Movies.title)
+    result = []
+    for i in by_title:
+        if isinstance (i, Series) == False:
+            result.append(i)
+            print(i.title)
+    return result
+
+def get_series():
+    by_title = sorted(ms, key=lambda Series: Series.title)
+    result = []
+    for i in by_title:
+        if isinstance (i, Series) == True:
+            result.append(i)
+            print(i.title)
+    return result
+
+def search():
+    t = input("Wprowadź tytuł filmu lub serialu którego szukasz:")
+    for i in ms:
+        if i.lower() == t.lower():
+            print(i)
+            #print("%s został znaleziony " %(t))
+        else:
+            print("%s nie został znaleziony " %(t()))
+
+get_movies()
+get_series()
+search()
